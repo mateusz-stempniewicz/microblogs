ccb98a9c9f4ed3e6779914ee573e753ef56e5b67
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2019-01-14 09:18:50 +0100

    add theme and aws-amplify components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import { withAuthenticator } from 'aws-amplify-react';
+
+import theme from './style/theme';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple - system,
+    BlinkMacSystemFont,
+    "Segoe UI",
+    "Roboto",
+    "Oxygen",
+    "Ubuntu",
+    "Cantarell",
+    "Fira Sans",
+    "Droid Sans",
+    "Helvetica Neue",
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <ThemeProvider theme={theme}>
+        <React.Fragment>
+          <GlobalStyle />
+          App
+        </React.Fragment>
+      </ThemeProvider>
     );
   }
 }
 
-export default App;
+export default withAuthenticator(App, true);
