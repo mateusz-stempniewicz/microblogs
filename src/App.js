e7846614465d2be2bea2941366ed026928d8bf8e
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2019-01-22 09:14:34 +0100

    add GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,40 +1,17 @@
-import React, { Component } from 'react';
-import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { withAuthenticator } from 'aws-amplify-react';
 
 import theme from './style/theme';
+import GlobalStyle from './style/GlobalStyle';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: -apple - system,
-    BlinkMacSystemFont,
-    "Segoe UI",
-    "Roboto",
-    "Oxygen",
-    "Ubuntu",
-    "Cantarell",
-    "Fira Sans",
-    "Droid Sans",
-    "Helvetica Neue",
-    sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-`;
-
-class App extends Component {
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-        <React.Fragment>
-          <GlobalStyle />
-          App
-        </React.Fragment>
-      </ThemeProvider>
-    );
-  }
-}
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <React.Fragment>
+      <GlobalStyle />
+      App
+    </React.Fragment>
+  </ThemeProvider>
+);
 
 export default withAuthenticator(App, true);
