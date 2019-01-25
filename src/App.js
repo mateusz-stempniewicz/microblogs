ba76cc9a650655d9c9790862854898f9dd9953bc
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2019-01-25 10:15:48 +0100

    add react-router-dom, config router, add homePage and dashboardPage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { withAuthenticator } from 'aws-amplify-react';
 
 import theme from './style/theme';
 import GlobalStyle from './style/GlobalStyle';
 
+const HomePage = lazy(() => import('./pages/HomePage'));
+const DashboardPage = lazy(() => import('./pages/DashboardPage'));
+
 const App = () => (
   <ThemeProvider theme={theme}>
-    <React.Fragment>
-      <GlobalStyle />
-      App
-    </React.Fragment>
+    <Router>
+      <React.Fragment>
+        <GlobalStyle />
+        <Suspense fallback={''}>
+          <Route exact path="/" render={() => <HomePage />} />
+          <Route exact path="/dashboard" render={() => <DashboardPage />} />
+        </Suspense>
+      </React.Fragment>
+    </Router>
   </ThemeProvider>
 );
 
